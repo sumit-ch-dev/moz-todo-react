349eaf84ea0597b9e259419e2a2179ee0cb8602b
symit1729 <symit1729@outlook.com>
2022-09-18 01:30:57 +0600

    delete task added

--- src/App.js
+++ src/App.js
@@ -23,6 +23,11 @@ function App(props) {
     });
     setTasks(updatedTasks);
   }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id != task.id);
+    setTasks(remainingTasks);
+  }
   const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
@@ -30,6 +35,7 @@ function App(props) {
       completed={task.completed}
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
 
