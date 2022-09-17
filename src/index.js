dbbdf4b659ce1816585a94ae6f4c9bb3e3ca2124
symit1729 <symit1729@outlook.com>
2022-09-17 17:02:34 +0600

    compartmetalized form and filterbutton

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+  { id: "todo-0", name:"Eat", completed: true},
+  { id: "todo-1", name:"Sleep", completed: false},
+  { id: "todo-2", name:"Repeat", completed: true},
+]
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App subject="Clarice"/>
+    <App tasks={DATA}/>
   </React.StrictMode>
 );
 
