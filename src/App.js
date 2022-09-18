5ee66123c96fadec97c15f24605ed3bacd5b9ca7
symit1729 <symit1729@outlook.com>
2022-09-18 14:00:07 +0600

    edit button working

--- src/App.js
+++ src/App.js
@@ -24,6 +24,17 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      /// if this task has the same ID as the edited task
+      if(id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   function deleteTask(id) {
     const remainingTasks = tasks.filter((task) => id != task.id);
     setTasks(remainingTasks);
@@ -36,6 +47,7 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
