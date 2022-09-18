972f56795cc725bc5a778939177d7cbf023c56f6
symit1729 <symit1729@outlook.com>
2022-09-18 14:53:23 +0600

    task filtering enabled

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 
 
 function App(props) {
 
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   function toggleTaskCompleted(id) {
-    const updatedTasks = tasks.map((task) =>{
+    const updatedTasks = tasks.map((task) => {
       //if this task has the same ID as the edited task
-      if(id === task.id) {
+      if (id === task.id) {
         // use object spread to make a new object
         // whose `completed` prop has been inverted
-        return {...task, completed: !task.completed}
+        return { ...task, completed: !task.completed }
       }
       return task;
     });
@@ -27,8 +35,8 @@ function App(props) {
   function editTask(id, newName) {
     const editedTaskList = tasks.map((task) => {
       /// if this task has the same ID as the edited task
-      if(id === task.id) {
-        return {...task, name: newName}
+      if (id === task.id) {
+        return { ...task, name: newName }
       }
       return task;
     });
@@ -39,17 +47,31 @@ function App(props) {
     const remainingTasks = tasks.filter((task) => id != task.id);
     setTasks(remainingTasks);
   }
-  const taskList = tasks.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
+
+
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
@@ -63,9 +85,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
 
