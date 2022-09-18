0a02c80cd1dd1c817681f44240c01b2b2a2b1b4d
symit1729 <symit1729@outlook.com>
2022-09-18 15:39:04 +0600

    focus updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { nanoid } from "nanoid";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 
 const FILTER_MAP = {
   All: () => true,
@@ -80,6 +89,18 @@ function App(props) {
 
   const tasksNoun = taskList.length <= 1 ? 'task' : 'tasks';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+  
+
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -87,7 +108,7 @@ function App(props) {
       <div className="filters btn-group stack-exception">
         {filterList}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef} >{headingText}</h2>
 
       <ul
         role="list"
